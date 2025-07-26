fe462d163dc42c1eab2a7ce1435da01853e261b7
zx <gcoordinate@gmail.com>
2025-07-26 23:56:26 +0800

    。。。

--- AListLib/scripts/install_gomobile.sh
+++ AListLib/scripts/install_gomobile.sh
@@ -1,5 +1,4 @@
 cd ../sources
-go mod download golang.org/x/mobile
-go install golang.org/x/mobile/cmd/gomobile
-go get golang.org/x/mobile/bind
+go install golang.org/x/mobile/cmd/gomobile@latest
 gomobile init
+go get golang.org/x/mobile/bind
